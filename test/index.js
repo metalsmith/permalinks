ebd145b32bf06fb6826a975330d3a542725a15b6
erikzaadi <erik.zaadi@gmail.com>
2014-03-17 20:52:13 +0200

    Altered date formatting to be trigger also by key

--- test/index.js
+++ test/index.js
@@ -11,79 +11,90 @@ describe('metalsmith-permalinks', function(){
 
   it('should change files even with no pattern', function(done){
     Metalsmith('test/fixtures/no-pattern')
-      .use(permalinks())
-      .build(function(err){
-        if (err) return done(err);
-        equal('test/fixtures/no-pattern/expected', 'test/fixtures/no-pattern/build');
-        done();
-      });
+    .use(permalinks())
+    .build(function(err){
+      if (err) return done(err);
+      equal('test/fixtures/no-pattern/expected', 'test/fixtures/no-pattern/build');
+      done();
+    });
 
   });
 
   it('should replace a pattern', function(done){
     Metalsmith('test/fixtures/pattern')
-      .use(permalinks({ pattern: ':title' }))
-      .build(function(err){
-        if (err) return done(err);
-        equal('test/fixtures/pattern/expected', 'test/fixtures/pattern/build');
-        done();
-      });
+    .use(permalinks({ pattern: ':title' }))
+    .build(function(err){
+      if (err) return done(err);
+      equal('test/fixtures/pattern/expected', 'test/fixtures/pattern/build');
+      done();
+    });
 
   });
 
   it('should accepts a shorthand string', function(done){
     Metalsmith('test/fixtures/shorthand')
-      .use(permalinks(':title'))
-      .build(function(err){
-        if (err) return done(err);
-        equal('test/fixtures/shorthand/expected', 'test/fixtures/shorthand/build');
-        done();
-      });
+    .use(permalinks(':title'))
+    .build(function(err){
+      if (err) return done(err);
+      equal('test/fixtures/shorthand/expected', 'test/fixtures/shorthand/build');
+      done();
+    });
 
   });
 
   it('should copy relative files to maintain references', function(done){
     Metalsmith('test/fixtures/relative')
-      .use(permalinks())
-      .build(function(err){
-        if (err) return done(err);
-        equal('test/fixtures/relative/expected', 'test/fixtures/relative/build');
-        done();
-      });
+    .use(permalinks())
+    .build(function(err){
+      if (err) return done(err);
+      equal('test/fixtures/relative/expected', 'test/fixtures/relative/build');
+      done();
+    });
   });
 
   it('should copy relative files even with patterns', function(done){
     Metalsmith('test/fixtures/relative-pattern')
-      .use(permalinks(':title'))
-      .build(function(err){
-        if (err) return done(err);
-        equal('test/fixtures/relative-pattern/expected', 'test/fixtures/relative-pattern/build');
-        done();
-      });
+    .use(permalinks(':title'))
+    .build(function(err){
+      if (err) return done(err);
+      equal('test/fixtures/relative-pattern/expected', 'test/fixtures/relative-pattern/build');
+      done();
+    });
   });
 
   it('should format a date', function(done){
     Metalsmith('test/fixtures/date')
-      .use(permalinks(':date'))
-      .build(function(err){
-        if (err) return done(err);
-        equal('test/fixtures/date/expected', 'test/fixtures/date/build');
-        done();
-      });
+    .use(permalinks(':date'))
+    .build(function(err){
+      if (err) return done(err);
+      equal('test/fixtures/date/expected', 'test/fixtures/date/build');
+      done();
+    });
 
   });
 
   it('should format a date with a custom formatter', function(done){
     Metalsmith('test/fixtures/custom-date')
-      .use(permalinks({
-        pattern: ':date',
-        date: 'YYYY/MM'
-      }))
-      .build(function(err){
-        if (err) return done(err);
-        equal('test/fixtures/custom-date/expected', 'test/fixtures/custom-date/build');
-        done();
-      });
+    .use(permalinks({
+      pattern: ':date',
+      date: 'YYYY/MM'
+    }))
+    .build(function(err){
+      if (err) return done(err);
+      equal('test/fixtures/custom-date/expected', 'test/fixtures/custom-date/build');
+      done();
+    });
+  });
 
+  it('should format a date when date saved as a string', function(done){
+    Metalsmith('test/fixtures/date-by-key')
+    .use(permalinks({
+      pattern: ':date'
+    }))
+    .build(function(err){
+      if (err) return done(err);
+      equal('test/fixtures/date-by-key/expected', 'test/fixtures/date-by-key/build');
+      done();
+    });
   });
 });
