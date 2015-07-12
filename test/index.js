ccdf85b4f3072d4686aaab32fa2dffb45b4de2eb
Simon Lepel <simbo@simbo.de>
2015-07-12 12:55:38 +0200

    use 'slug' instead of 'slug-component' for slugifying; accept slug options

--- test/index.js
+++ test/index.js
@@ -136,4 +136,20 @@ describe('metalsmith-permalinks', function(){
       });
   });
 
+  it('should accept options for slug module', function(done){
+    Metalsmith('test/fixtures/slug-options')
+      .use(permalinks({
+        pattern: ':title',
+        slug: {
+          remove: /[.]/g,
+          lower: false
+        }
+      }))
+      .build(function(err){
+        if (err) return done(err);
+        equal('test/fixtures/slug-options/expected', 'test/fixtures/slug-options/build');
+        done();
+      });
+  });
+
 });
